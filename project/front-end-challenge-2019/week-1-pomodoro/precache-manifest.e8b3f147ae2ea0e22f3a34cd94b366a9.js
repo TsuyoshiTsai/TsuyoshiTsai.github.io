7fc9cb7341dd4f9aa425a7732119b923f4275cb1
Patrick <patrick3948@gmail.com>
2019-07-20 03:57:37 +0800

    Update pomodoro project public file

---
+++ project/front-end-challenge-2019/week-1-pomodoro/precache-manifest.e8b3f147ae2ea0e22f3a34cd94b366a9.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abbf4c86c24e4e4c945c6fa52399131e",
+    "url": "/project/front-end-challenge-2019/week-1-pomodoro/index.html"
+  },
+  {
+    "revision": "c2b43d9569a4ccc36910",
+    "url": "/project/front-end-challenge-2019/week-1-pomodoro/static/css/4.1866cab8.chunk.css"
+  },
+  {
+    "revision": "505b56f1cc2b2210501e",
+    "url": "/project/front-end-challenge-2019/week-1-pomodoro/static/css/5.109e01d8.chunk.css"
+  },
+  {
+    "revision": "7e63151544fa865da5c4",
+    "url": "/project/front-end-challenge-2019/week-1-pomodoro/static/css/6.0d185f62.chunk.css"
+  },
+  {
+    "revision": "7d41db27dbb359ba5dec",
+    "url": "/project/front-end-challenge-2019/week-1-pomodoro/static/css/8.81ab598c.chunk.css"
+  },
+  {
+    "revision": "40844fd4bd4430a74412",
+    "url": "/project/front-end-challenge-2019/week-1-pomodoro/static/css/main.9c7e3767.chunk.css"
+  },
+  {
+    "revision": "ff28dfc19e943c81c41d",
+    "url": "/project/front-end-challenge-2019/week-1-pomodoro/static/js/0.a8ab5b46.chunk.js"
+  },
+  {
+    "revision": "28be0c4bee66ef3dbbc9",
+    "url": "/project/front-end-challenge-2019/week-1-pomodoro/static/js/3.85b3f62e.chunk.js"
+  },
+  {
+    "revision": "c2b43d9569a4ccc36910",
+    "url": "/project/front-end-challenge-2019/week-1-pomodoro/static/js/4.32321abf.chunk.js"
+  },
+  {
+    "revision": "505b56f1cc2b2210501e",
+    "url": "/project/front-end-challenge-2019/week-1-pomodoro/static/js/5.43079529.chunk.js"
+  },
+  {
+    "revision": "7e63151544fa865da5c4",
+    "url": "/project/front-end-challenge-2019/week-1-pomodoro/static/js/6.7be0350d.chunk.js"
+  },
+  {
+    "revision": "3fa7dd6d00374d1e596f",
+    "url": "/project/front-end-challenge-2019/week-1-pomodoro/static/js/7.ff7ecd80.chunk.js"
+  },
+  {
+    "revision": "7d41db27dbb359ba5dec",
+    "url": "/project/front-end-challenge-2019/week-1-pomodoro/static/js/8.4b0a019d.chunk.js"
+  },
+  {
+    "revision": "40844fd4bd4430a74412",
+    "url": "/project/front-end-challenge-2019/week-1-pomodoro/static/js/main.c2df07ae.chunk.js"
+  },
+  {
+    "revision": "a8d029045c803a763bdc",
+    "url": "/project/front-end-challenge-2019/week-1-pomodoro/static/js/runtime~main.50c1af32.js"
+  },
+  {
+    "revision": "b84fccdc8556af860c81922684d0e01e",
+    "url": "/project/front-end-challenge-2019/week-1-pomodoro/static/media/Alarm_Clock.b84fccdc.mp3"
+  },
+  {
+    "revision": "0063e353c2c14f8b6316030298a882dd",
+    "url": "/project/front-end-challenge-2019/week-1-pomodoro/static/media/Beep_Short.0063e353.mp3"
+  },
+  {
+    "revision": "f244d2ae1e079271ea6685700fa76fe7",
+    "url": "/project/front-end-challenge-2019/week-1-pomodoro/static/media/Check.f244d2ae.svg"
+  },
+  {
+    "revision": "ebb9f2a989451b891d9d5a8a6df416df",
+    "url": "/project/front-end-challenge-2019/week-1-pomodoro/static/media/Mechanical_Clock_Ring.ebb9f2a9.mp3"
+  },
+  {
+    "revision": "0328f8c81620712329b69ae0e30f08c0",
+    "url": "/project/front-end-challenge-2019/week-1-pomodoro/static/media/Medium_Bell_Ringing_Near.0328f8c8.mp3"
+  },
+  {
+    "revision": "a10bc4f48d27d73d916faeee8da71da0",
+    "url": "/project/front-end-challenge-2019/week-1-pomodoro/static/media/Phone_Alerts_And_Rings.a10bc4f4.mp3"
+  },
+  {
+    "revision": "317ed8ae752401f0c96788d09f46e8cc",
+    "url": "/project/front-end-challenge-2019/week-1-pomodoro/static/media/Tomato.317ed8ae.svg"
+  }
+]);
--- project/front-end-challenge-2019/week-1-pomodoro/service-worker.js
+++ project/front-end-challenge-2019/week-1-pomodoro/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f6aa44d1b9865adb4c1a22cea28c15ce.js"
+  "/project/front-end-challenge-2019/week-1-pomodoro/precache-manifest.e8b3f147ae2ea0e22f3a34cd94b366a9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/project/front-end-challenge-2019/week-1-pomodoro/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
