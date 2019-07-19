fc18665188f61f8466d383adf7e3b0f26bd957fa
Patrick <patrick3948@gmail.com>
2019-07-20 03:19:20 +0800

    Add project front-end-challenge-2019/week-1-pomodoro

---
+++ project/front-end-challenge-2019/week-1-pomodoro/precache-manifest.f6aa44d1b9865adb4c1a22cea28c15ce.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cbb5f40e05e114649badbaa506ea877",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2b43d9569a4ccc36910",
+    "url": "/static/css/4.1866cab8.chunk.css"
+  },
+  {
+    "revision": "505b56f1cc2b2210501e",
+    "url": "/static/css/5.109e01d8.chunk.css"
+  },
+  {
+    "revision": "7e63151544fa865da5c4",
+    "url": "/static/css/6.0d185f62.chunk.css"
+  },
+  {
+    "revision": "7d41db27dbb359ba5dec",
+    "url": "/static/css/8.81ab598c.chunk.css"
+  },
+  {
+    "revision": "d71e9a73c0668d9c31af",
+    "url": "/static/css/main.9c7e3767.chunk.css"
+  },
+  {
+    "revision": "ff28dfc19e943c81c41d",
+    "url": "/static/js/0.a8ab5b46.chunk.js"
+  },
+  {
+    "revision": "28be0c4bee66ef3dbbc9",
+    "url": "/static/js/3.85b3f62e.chunk.js"
+  },
+  {
+    "revision": "c2b43d9569a4ccc36910",
+    "url": "/static/js/4.32321abf.chunk.js"
+  },
+  {
+    "revision": "505b56f1cc2b2210501e",
+    "url": "/static/js/5.43079529.chunk.js"
+  },
+  {
+    "revision": "7e63151544fa865da5c4",
+    "url": "/static/js/6.7be0350d.chunk.js"
+  },
+  {
+    "revision": "3fa7dd6d00374d1e596f",
+    "url": "/static/js/7.ff7ecd80.chunk.js"
+  },
+  {
+    "revision": "7d41db27dbb359ba5dec",
+    "url": "/static/js/8.4b0a019d.chunk.js"
+  },
+  {
+    "revision": "d71e9a73c0668d9c31af",
+    "url": "/static/js/main.16662699.chunk.js"
+  },
+  {
+    "revision": "f2e726079dd314628fdc",
+    "url": "/static/js/runtime~main.3c120071.js"
+  },
+  {
+    "revision": "b84fccdc8556af860c81922684d0e01e",
+    "url": "/static/media/Alarm_Clock.b84fccdc.mp3"
+  },
+  {
+    "revision": "0063e353c2c14f8b6316030298a882dd",
+    "url": "/static/media/Beep_Short.0063e353.mp3"
+  },
+  {
+    "revision": "f244d2ae1e079271ea6685700fa76fe7",
+    "url": "/static/media/Check.f244d2ae.svg"
+  },
+  {
+    "revision": "ebb9f2a989451b891d9d5a8a6df416df",
+    "url": "/static/media/Mechanical_Clock_Ring.ebb9f2a9.mp3"
+  },
+  {
+    "revision": "0328f8c81620712329b69ae0e30f08c0",
+    "url": "/static/media/Medium_Bell_Ringing_Near.0328f8c8.mp3"
+  },
+  {
+    "revision": "a10bc4f48d27d73d916faeee8da71da0",
+    "url": "/static/media/Phone_Alerts_And_Rings.a10bc4f4.mp3"
+  },
+  {
+    "revision": "317ed8ae752401f0c96788d09f46e8cc",
+    "url": "/static/media/Tomato.317ed8ae.svg"
+  }
+]);
