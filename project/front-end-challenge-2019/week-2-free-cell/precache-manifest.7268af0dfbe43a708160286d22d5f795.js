d4d6f455319485a4af0877cfe5aa0060f333c4a8
Patrick <patrick3948@gmail.com>
2019-07-29 01:28:53 +0800

    Add free-cell project

---
+++ project/front-end-challenge-2019/week-2-free-cell/precache-manifest.7268af0dfbe43a708160286d22d5f795.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d728c53c1f0cdfa8ca00371c2a1756d",
+    "url": "/project/front-end-challenge-2019/week-2-free-cell/index.html"
+  },
+  {
+    "revision": "b25c6db7d5a17b60b81a",
+    "url": "/project/front-end-challenge-2019/week-2-free-cell/static/css/main.f25d2a2c.chunk.css"
+  },
+  {
+    "revision": "bb2ccf14fb4518bff0c0",
+    "url": "/project/front-end-challenge-2019/week-2-free-cell/static/js/2.adea8de9.chunk.js"
+  },
+  {
+    "revision": "b25c6db7d5a17b60b81a",
+    "url": "/project/front-end-challenge-2019/week-2-free-cell/static/js/main.066c4e4d.chunk.js"
+  },
+  {
+    "revision": "47b32736612c702ad7c1",
+    "url": "/project/front-end-challenge-2019/week-2-free-cell/static/js/runtime~main.f4979aa1.js"
+  },
+  {
+    "revision": "c00b29193232eea3d28dfe999e14a296",
+    "url": "/project/front-end-challenge-2019/week-2-free-cell/static/media/club10.c00b2919.png"
+  },
+  {
+    "revision": "8a182d914c6d5371b1bd06384821ccbf",
+    "url": "/project/front-end-challenge-2019/week-2-free-cell/static/media/club11.8a182d91.png"
+  },
+  {
+    "revision": "b5bd79660e4029db4103693b9f3f55d1",
+    "url": "/project/front-end-challenge-2019/week-2-free-cell/static/media/club12.b5bd7966.png"
+  },
+  {
+    "revision": "29e619976ffb42df42424fe734d8f11d",
+    "url": "/project/front-end-challenge-2019/week-2-free-cell/static/media/club13.29e61997.png"
+  },
+  {
+    "revision": "f05e3ebe89f668aaa13c1a3dfb796198",
+    "url": "/project/front-end-challenge-2019/week-2-free-cell/static/media/club6.f05e3ebe.png"
+  },
+  {
+    "revision": "623c2586d1a52fa7677dc62e7804617d",
+    "url": "/project/front-end-challenge-2019/week-2-free-cell/static/media/club7.623c2586.png"
+  },
+  {
+    "revision": "9cd60a4f5d6d599980b59b05fc0e55c9",
+    "url": "/project/front-end-challenge-2019/week-2-free-cell/static/media/club8.9cd60a4f.png"
+  },
+  {
+    "revision": "12dcf016badd4f74f1411e1b1235977f",
+    "url": "/project/front-end-challenge-2019/week-2-free-cell/static/media/club9.12dcf016.png"
+  },
+  {
+    "revision": "504d514302aefc8392ee98cf05946dcb",
+    "url": "/project/front-end-challenge-2019/week-2-free-cell/static/media/diamond11.504d5143.png"
+  },
+  {
+    "revision": "ca7224c1b826b535351429d73c2241e2",
+    "url": "/project/front-end-challenge-2019/week-2-free-cell/static/media/diamond12.ca7224c1.png"
+  },
+  {
+    "revision": "b63ae66c815b272e3f8a85149f40fa07",
+    "url": "/project/front-end-challenge-2019/week-2-free-cell/static/media/diamond13.b63ae66c.png"
+  },
+  {
+    "revision": "2c42ddf6b4f141ecba0fd7f0e70b1a53",
+    "url": "/project/front-end-challenge-2019/week-2-free-cell/static/media/heart11.2c42ddf6.png"
+  },
+  {
+    "revision": "77a9592458f8170b2f571f998a35170f",
+    "url": "/project/front-end-challenge-2019/week-2-free-cell/static/media/heart12.77a95924.png"
+  },
+  {
+    "revision": "68815d8382891dc30835a4034648aaec",
+    "url": "/project/front-end-challenge-2019/week-2-free-cell/static/media/heart13.68815d83.png"
+  },
+  {
+    "revision": "c6c4594ae15a2ce4d7c152fd459617fd",
+    "url": "/project/front-end-challenge-2019/week-2-free-cell/static/media/spade10.c6c4594a.png"
+  },
+  {
+    "revision": "69fcfad9ad29853d17f42479faa8f093",
+    "url": "/project/front-end-challenge-2019/week-2-free-cell/static/media/spade11.69fcfad9.png"
+  },
+  {
+    "revision": "ed45de2d26e40127ed754a01355c7fb4",
+    "url": "/project/front-end-challenge-2019/week-2-free-cell/static/media/spade12.ed45de2d.png"
+  },
+  {
+    "revision": "c065b41f55a87a3f6951ae5ee6b3e322",
+    "url": "/project/front-end-challenge-2019/week-2-free-cell/static/media/spade13.c065b41f.png"
+  },
+  {
+    "revision": "1a04ff4e3b4b04de6f6f6cd29c7a5d25",
+    "url": "/project/front-end-challenge-2019/week-2-free-cell/static/media/spade6.1a04ff4e.png"
+  },
+  {
+    "revision": "ce2774c3c9f7a2bed7db4bd1b9efadbf",
+    "url": "/project/front-end-challenge-2019/week-2-free-cell/static/media/spade7.ce2774c3.png"
+  },
+  {
+    "revision": "27953252070d2aadf6aa06de80968e1e",
+    "url": "/project/front-end-challenge-2019/week-2-free-cell/static/media/spade8.27953252.png"
+  },
+  {
+    "revision": "60517b57681c8a2344acdbb78576d5ad",
+    "url": "/project/front-end-challenge-2019/week-2-free-cell/static/media/spade9.60517b57.png"
+  }
+]);
