cb595d01e9ec504ebd0c1140a6871c2dbf334d0e
Patrick <patrick3948@gmail.com>
2019-08-05 01:21:44 +0800

    Update week-3-mp3-player

---
+++ project/front-end-challenge-2019/week-3-mp3-player/precache-manifest.1783818074ad2027a52c7c8c1d731fad.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f25795a63952ad626ad99feaca153ccf",
+    "url": "/project/front-end-challenge-2019/week-3-mp3-player/index.html"
+  },
+  {
+    "revision": "1b2982c83bc4f3b89b1e",
+    "url": "/project/front-end-challenge-2019/week-3-mp3-player/static/css/main.5368ff5a.chunk.css"
+  },
+  {
+    "revision": "cb04b37770767af359c4",
+    "url": "/project/front-end-challenge-2019/week-3-mp3-player/static/js/2.189f6d44.chunk.js"
+  },
+  {
+    "revision": "1b2982c83bc4f3b89b1e",
+    "url": "/project/front-end-challenge-2019/week-3-mp3-player/static/js/main.65cdabbb.chunk.js"
+  },
+  {
+    "revision": "0ed624cc90b2f9decfc4",
+    "url": "/project/front-end-challenge-2019/week-3-mp3-player/static/js/runtime~main.77c68f25.js"
+  },
+  {
+    "revision": "c16c35d592377e4b6d906be443027ea8",
+    "url": "/project/front-end-challenge-2019/week-3-mp3-player/static/media/For_We_Are_Many.c16c35d5.mp3"
+  },
+  {
+    "revision": "5d340e5c89cb7def3eef7294c6a54c6a",
+    "url": "/project/front-end-challenge-2019/week-3-mp3-player/static/media/Metamorphosis.5d340e5c.mp3"
+  },
+  {
+    "revision": "392e0a136fee08525fa570d0357188a8",
+    "url": "/project/front-end-challenge-2019/week-3-mp3-player/static/media/Slowly_Until_We_Get_There.392e0a13.mp3"
+  },
+  {
+    "revision": "e0a62affad9445c2069a7fef079f95fe",
+    "url": "/project/front-end-challenge-2019/week-3-mp3-player/static/media/Spirit_of_the_Dead.e0a62aff.mp3"
+  },
+  {
+    "revision": "f45f78f41d2a7507f48bdea101d2577e",
+    "url": "/project/front-end-challenge-2019/week-3-mp3-player/static/media/That_Never_Dies.f45f78f4.mp3"
+  },
+  {
+    "revision": "b28de11cd5df333e3e91e9750100aa3e",
+    "url": "/project/front-end-challenge-2019/week-3-mp3-player/static/media/The_Long_Night.b28de11c.mp3"
+  },
+  {
+    "revision": "34058d925a2d072870ce052ccd4f3b19",
+    "url": "/project/front-end-challenge-2019/week-3-mp3-player/static/media/Wishful_Thinking.34058d92.mp3"
+  }
+]);
--- project/front-end-challenge-2019/week-3-mp3-player/service-worker.js
+++ project/front-end-challenge-2019/week-3-mp3-player/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/project/front-end-challenge-2019/week-3-map3-player/precache-manifest.ccfab41fcd65ca83b38deb52a96fa835.js"
+  "/project/front-end-challenge-2019/week-3-mp3-player/precache-manifest.1783818074ad2027a52c7c8c1d731fad.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/project/front-end-challenge-2019/week-3-map3-player/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/project/front-end-challenge-2019/week-3-mp3-player/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
